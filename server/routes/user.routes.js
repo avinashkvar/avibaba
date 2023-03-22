405a2b7c8cd310374cf651bf7c25d7a8f92d8e00
avinashkvar <k3076183@gmail.com>
2023-03-22 12:13:40 +0530

    getting user

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -3,6 +3,28 @@ const { register, login } = require('../controllers/user.controllers');
 
 const userRoute = express.Router();
 
+userRoute.get('/user/loginUser', (req, res) => {
+	const authorization = req.headers.authorization;
+
+	if (authorization) {
+		let token = authorization.split(' ').pop();
+		try {
+			if (token) {
+				jwt.verify(token, process.env.JWT_SECRET);
+
+				const user = jwt.decode(token);
+				return res.send(user);
+			} else {
+				return res.status(200).send({ error: 'invalid token provided' });
+			}
+		} catch (error) {
+			return res.status(200).send({error});
+		}
+	} else {
+		return res.status(200).send({ error: 'no token provided' });
+	}
+});
+
 userRoute.post('/register', async (req, res) => {
 	const body = req.body;
 
